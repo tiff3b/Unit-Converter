db804bb1c1e0f827d535a873b4ce89a2850b10c6
tiff3b <tiffany3berger@gmail.com>
2025-11-29 15:09:54 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+let numberInput = document.getElementById("number")
+const convertBtn = document.getElementById("convert")
+let length = document.getElementById("length-convert")
+let volume = document.getElementById("volume-convert")
+let mass = document.getElementById("mass-convert")
+
+
+convertBtn.addEventListener("click", function (){
+   
+
+})
