c910baa6b4f9a2e283e787bf832d90470b0c1eb5
tiff3b <tiffany3berger@gmail.com>
2025-11-30 12:48:29 -0500

    Added conversion chart for convert options

--- index.js
+++ index.js
@@ -10,7 +10,28 @@ let volume = document.getElementById("volume-convert")
 let mass = document.getElementById("mass-convert")
 
 
-convertBtn.addEventListener("click", function (){
-   
 
+convertBtn.addEventListener("click", function (){
+    const numberValue = numberInput.value     
+    const parsedNumber = parseFloat(numberValue)
+    if (!isNaN(parsedNumber)){
+        const meter = parsedNumber
+        const feet = meter * 3.281
+        const feetToMeters = parsedNumber / 3.281
+        length.innerHTML = `
+            ${numberValue} meters = ${feet.toFixed(3)} feet | 
+            ${numberValue} feet = ${feetToMeters.toFixed(3)} meters`
+        const liter = parsedNumber
+        const gallon = liter * 0.264
+        const gallonToLiters = parsedNumber / 0.264
+        volume.innerHTML = `
+            ${numberValue} liters = ${gallon.toFixed(3)} gallons | 
+            ${numberValue} gallons = ${gallonToLiters.toFixed(3)} liters`
+        const kilos = parsedNumber
+        const pounds = kilos * 2.204
+        const poundsToKilos = parsedNumber / 2.204
+        mass.innerHTML = `
+            ${numberValue} kilos = ${pounds.toFixed(3)} pounds | 
+            ${numberValue} pounds = ${poundsToKilos.toFixed(3)} kilos`
+    }
 })
